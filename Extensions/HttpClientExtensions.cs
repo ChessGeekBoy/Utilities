fe25e8f70f54308c06f80fc64af5fb2f91974c6a
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-04-12 07:43:30 -0700

    Added all HTTP methods to HttpClientExtensions.cs

--- Extensions/HttpClientExtensions.cs
+++ Extensions/HttpClientExtensions.cs
@@ -11,9 +11,29 @@ namespace Utilities.Extensions
 {
     public static class HttpClientExtensions
     {
-        public static string GetSynchronously(this HttpClient httpClient, Uri uri)
+        public static string Get(this HttpClient httpClient, Uri requestUri)
         {
-            return httpClient.GetStringAsync(uri).Result;
+            return httpClient.GetStringAsync(requestUri).Result;
+        }
+
+        public static void Post(this HttpClient httpClient, Uri requestUri, HttpContent requestContent)
+        {
+            httpClient.PostAsync(requestUri, requestContent).Wait();
+        }
+
+        public static void Patch(this HttpClient httpClient, Uri requestUri, HttpContent requestContent)
+        {
+            httpClient.PatchAsync(requestUri, requestContent).Wait();
+        }
+
+        public static void Put(this HttpClient httpClient, Uri requesturi, HttpContent requestContent)
+        {
+            httpClient.PutAsync(requesturi, requestContent).Wait();
+        }
+
+        public static void Delete(this HttpClient httpClient, Uri requestUri)
+        {
+            httpClient.DeleteAsync(requestUri).Wait();
         }
     }
 }
