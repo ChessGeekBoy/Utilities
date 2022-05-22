da34d6e5f959119fcf325d175050269c6122b713
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-05-22 12:10:30 -0700

    Started on Statistics

---
+++ Cmdlets/Statistics/GetMeanCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace Utilities.Cmdlets.Statistics
+{
+    [Cmdlet(VerbsCommon.Get, "Mean", HelpUri = "https://github.com/ChessGeekBoy/Utilities/tree/main/docs/Cmdlets/Statistics/Get-Mean.md")]
+    public class GetMeanCommand : PSCmdlet
+    {
+        [Parameter(Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            HelpMessage = "The data from which the mean should be found.")]
+        public IEnumerable<decimal> Data { get; set; }
+
+        protected override void EndProcessing()
+        {
+            decimal mean = Utilities.Math.Statistics.GetMean(this.Data);
+            WriteObject(mean);
+        }
+    }
+}
---
+++ docs/Cmdlets/Statistics/Get-Mean.md
@@ -0,0 +1,13 @@
+﻿# `Get-Mean`
+## Parameters
+### `Data`
+An `IEnumerable<decimal>` from which the mean should be found.
+
+## Examples
+```powershell
+Get-Mean -Data @(1, 2, 3)
+```
+Output:
+```output
+2
+```
