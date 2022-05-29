2ef328aca1cf5c6f78c64c7d1da28b63daf9235a
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-05-28 18:31:50 -0700

    Fixed build errors by commenting out non-implemented methods.

--- Math/Statistics.cs
+++ Math/Statistics.cs
@@ -20,28 +20,28 @@ namespace Utilities.Math
             return sum / values.Count();
         }
 
-        public static decimal GetMode(IEnumerable<decimal> values)
-        {
-            Dictionary<decimal, int> frequencies = new Dictionary<decimal, int>();
+        //public static decimal GetMode(IEnumerable<decimal> values)
+        //{
+        //    Dictionary<decimal, int> frequencies = new Dictionary<decimal, int>();
 
-            foreach (decimal value in values)
-            {
-                if (frequencies.ContainsKey(value))
-                {
-                    frequencies[value]++;
-                }
-                else
-                {
-                    frequencies.Add(value, 1);
-                }
-            }
+        //    foreach (decimal value in values)
+        //    {
+        //        if (frequencies.ContainsKey(value))
+        //        {
+        //            frequencies[value]++;
+        //        }
+        //        else
+        //        {
+        //            frequencies.Add(value, 1);
+        //        }
+        //    }
 
             
-        }
+        //}
 
-        public static decimal GetMedian(IEnumerable<decimal> values)
-        {
+        //public static decimal GetMedian(IEnumerable<decimal> values)
+        //{
             
-        }
+        //}
     }
 }
